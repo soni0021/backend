689161b63b0bc2be9e3845b888754cdb57295041
soni0021 <bt22cse027@iiitn.ac.in>
2025-02-17 18:56:50 +0530

    structure routes

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+
+  app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+
+
+  app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+
+  app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+  app.listen(3000);
